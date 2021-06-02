ed617848f90569bf658e768c8233d2eec076b4a4
Christopher Kline <58607806+ckline-tryptic@users.noreply.github.com>
2021-06-02 18:53:50 +0000

    add tag to version, filter on ref_type

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import { Probot } from "probot";
-// import * as https from "https";
+import * as https from "https";
 import * as sax from "sax"
 
-// const CORGI_URL = "https://corgi-staging.openstax.org/api/jobs/"
+const CORGI_URL = "https://corgi-staging.openstax.org/api/jobs/"
 
 export = (app: Probot) => {
   app.on("create", async (context) => {
     // NOTE: if we miss webhooks look into persistence
 
     // Do we need to filter events?
-    app.log.info(context);
+    if (context.payload.ref_type !== 'tag') {
+      app.log.error(`ref_type '${context.payload.ref_type}' is not tag`);
+      return;
+    }
 
     // get books in repo
     const contentRequest = context.repo({ path: 'META-INF/books.xml' })
@@ -17,9 +20,9 @@ export = (app: Probot) => {
     const contentData = contentMetadata.data as any;
     const content = await context.octokit.request(contentData.download_url)
 
-    app.log.info(content.data);
+    // app.log.info(content.data);
 
-    // const repo = context.payload.repository.name
+    const repo = context.payload.repository.name
 
     var books: string[] = [];
     const parser = sax.parser();
@@ -32,44 +35,44 @@ export = (app: Probot) => {
 
     // send job details to CORGI API
     // make request per book + job type
-    // books.forEach(slug => {
-    //   [3, 4].forEach(jobType => {
-    //     // 3: git-pdf
-    //     // 4: git-distribution-preview
+    books.forEach(slug => {
+      [3, 4].forEach(jobType => {
+        // 3: git-pdf
+        // 4: git-distribution-preview
 
-    //     app.log.info(`collection_id: ${repo}/${slug}`)
-    //     app.log.info(`job_type_id: ${jobType}`)
+        app.log.info(`collection_id: ${repo}/${slug}`)
+        app.log.info(`job_type_id: ${jobType}`)
 
-    //     const options = {
-    //       method: 'POST',
-    //     }
+        const options = {
+          method: 'POST',
+        }
 
-    //     const request = https.request(CORGI_URL, options);
+        const request = https.request(CORGI_URL, options);
 
-    //     request.on('data', (d) => {
-    //       app.log.info(d);
-    //     });
+        request.on('data', (d) => {
+          app.log.info(d);
+        });
 
-    //     request.on('response', (r) => {
-    //       app.log.info(`${r.statusCode}`);
-    //     })
+        request.on('response', (r) => {
+          app.log.info(`${r.statusCode}`);
+        })
 
-    //     request.on('error', (e) => {
-    //       app.log.info(e);
-    //     });
+        request.on('error', (e) => {
+          app.log.info(e);
+        });
 
-    //     const data = {
-    //       collection_id: `${repo}/${slug}`,
-    //       job_type_id: jobType,
-    //       status_id: 1,
-    //       version: 'qa' // version from tag
-    //       // style: ???
-    //     }
+        const data = {
+          collection_id: `${repo}/${slug}`,
+          job_type_id: jobType,
+          status_id: 1,
+          version: `${context.payload.ref}`
+          // style: ???
+        }
 
-    //     request.write(JSON.stringify(data))
+        request.write(JSON.stringify(data))
 
-    //     request.end()
-    //   });
-    // });
+        request.end()
+      });
+    });
   });
 };
